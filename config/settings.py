3b0025256de9a4f5f013258e563ec7a3ede52641
kimhangyu <khk201@naver.com>
2024-12-15 23:22:19 +0900

    fix

--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,8 @@ import os
 from dotenv import load_dotenv
 import dj_database_url
 
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
