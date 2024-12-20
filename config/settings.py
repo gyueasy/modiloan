3dcbf0737732be6cd59be32652983244fc81e779
kimhangyu <khk201@naver.com>
2024-12-21 01:38:26 +0900

    hm

--- config/settings.py
+++ config/settings.py
@@ -97,21 +97,22 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    'SIMPLE_JWT': {
-        'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-        'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-        'ROTATE_REFRESH_TOKENS': True,
-        'BLACKLIST_AFTER_ROTATION': True,
-        'BLACKLIST_ENABLED': True,
-        'ALGORITHM': 'HS256',
-        # 'SIGNING_KEY': SECRET_KEY,
-        # 'VERIFYING_KEY': None,
-        # 'AUTH_HEADER_TYPES': ('Bearer',),
-        # 'USER_ID_FIELD': 'id',
-        # 'USER_ID_CLAIM': 'user_id',
-        # 'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-        # 'TOKEN_TYPE_CLAIM': 'token_type',
-    }
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'BLACKLIST_ENABLED': True,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
 }
 # JWT의 기본 설정 (옵션)
 # SIMPLE_JWT = {
